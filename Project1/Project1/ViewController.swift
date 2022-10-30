879d93951b2e5ae7005197f570299c19a7d9b77d
Omar Almasri <o_almasri@fanshaweonline.ca>
2022-10-30 16:47:42 -0400

    blank project

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by omar on 10/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
